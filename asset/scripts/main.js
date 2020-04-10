7cf800ad18502001a44872d749d49f2979f83581
xuwei <xuwei@dayuwuxian.com>
2020-04-10 14:25:28 +0800

    feature: rm lang judge

--- asset/scripts/main.js
+++ asset/scripts/main.js
@@ -11,10 +11,6 @@ function isInMobile() {
  */
 var _hmt = _hmt || [];
 (function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?dc2a1a0f208fdb13266eebc8b6278cb3";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
 })();
 
 /**
@@ -34,51 +30,6 @@ var _hmt = _hmt || [];
  * 检测英文版跳转
  */
 (function() {
-  var localS = window.localStorage;
-  var routePath = window.location.pathname;
-  var queryParams = window.location.search;
-  //第一次进入没有设置过语言环境，选用浏览器的语言
-  if( localS.getItem('lang') == null || localS.getItem('lang') == "" ){
-	  var lang = window.navigator.language || window.navigator.browserLanguage ;
-	  localS.setItem('lang', lang);
-  } else {
-	  var lang = localS.getItem('lang');
-  }
-  //判断语言类型
-  var isZhReg = /zh/i;
-  var isZhLang = false;
-  if(isZhReg.test(lang)){
-	  isZhLang = true;
-  }
-
-  
-  //判断路径类型
-  var routeIsEn = routePath.indexOf('/en') !== -1 || queryParams.indexOf('language=en') !== -1;
-  //博客详情页不做自动跳转
-  var isBlogDetailReg = /\d[1-9]/;
-  if(isBlogDetailReg.test(routePath) ){
-	  return;
-  }
-  
-  if(isZhLang && routeIsEn){  //中文语言环境 又输入的是英文路径
-	  //博客页面地址需要单独处理
-	  if (routePath.indexOf('/en/blog') == -1){
-		  location_path = routePath.replace("/en", '');
-		  if( location_path == ""){
-			  location_path = "/";
-		  }
-		  window.location = location_path ;
-	  }else{
-		  window.location = routePath.replace('/en/blog', '/category/blog');
-	  }
-  }else if( !isZhLang && !routeIsEn ){ //英文语言环境 又输入的是中文路径
-	  //博客页面地址需要单独处理,博客详情页面不跳转
-	  if (routePath.indexOf('/category/blog') == -1 ){
-		  window.location = '/en' + routePath;
-	  }else if(routePath.indexOf('/category/blog') !== -1){
-		  window.location = routePath.replace('/category/blog','/en/blog');
-	  }
-  }
 
 })();
 
